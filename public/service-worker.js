f9ca2ddaed9b3d2b3cdba8d5b3ab5a0962747168
Zachary Clark <zacclark369@gmail.com>
2022-03-24 16:20:17 -0700

    build quick standard service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,92 +1,73 @@
-const APP_PREFIX = 'MobileBudgetTracker';
-const VERSION = 'version_01';
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+const APP_PREFIX = "MobileBudgetTracker";
+const VERSION = "version_01";
 
-const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/favicon.ico',
-    '/manifest.json',
-    '/assets/css/style.css',
-    '/app.js',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
-   
-  ];
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((chach) => {
+      console.log("Your files were pre-cached successfully!");
+      return chache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
 
-  
-self.addEventListener('install', function(evt) {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then(chach => {
-            console.log('Your files were pre-cached successfully!');
-            return chache.addAll(FILES_TO_CACHE);
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Remover old cache data", key);
+            return caches.delete(key);
+          }
         })
-    );
-    self.skipWaiting();
-})
+      );
+    })
+  );
 
-// Activate the service worker and remove old data from the cache
-// YOUR CODE HERE
-
-self.addEventListener('activate', function(evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                    console.log('Remover old cache data', key);
-                    return caches.delete(key);
-                }
-                })
-        );
-            })
-);
-
-self.clients.claim();
-
-        });
+  self.clients.claim();
+});
 
 // Intercept fetch requests
 // YOUR CODE HERE
 
-self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.includes('/api/')) {
-        evt.respondWith(
-          caches
-            .open(DATA_CACHE_NAME)
-            .then(cache => {
-              return fetch(evt.request)
-                .then(response => {
-                  // If the response was good, clone it and store it in the cache.
-                  if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                  }
-    
-                  return response;
-                })
-                .catch(err => {
-                  // Network request failed, try to get it from the cache.
-                  return cache.match(evt.request);
-                });
-            })
-            .catch(err => console.log(err))
-        );
-    
-        return;
-      }
-      evt.respondWith(
-        fetch(evt.request).catch(function() {
-          return caches.match(evt.request).then(function(response) {
-            if (response) {
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
               return response;
-            } else if (evt.request.headers.get('accept').includes('text/html')) {
-              // return the cached home page for all requests for html pages
-              return caches.match('/');
-            }
-          });
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
         })
-      );
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
 });
