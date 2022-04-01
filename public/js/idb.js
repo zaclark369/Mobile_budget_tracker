17a45095462a5fd55198d79b2a24cc3deba2eed3
Zachary Clark <zacclark369@gmail.com>
2022-03-31 17:24:47 -0700

    fixes to service worker, idb, and index.html manifest connection

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,10 @@
+const indexedDB = 
+  window.indexedDB || 
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
 let db;
 const request = indexedDB.open('budget', 1);
 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = "MobileBudget-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-"+ VERSION;
 const FILES_TO_CACHE = ["./index.html", "./css/styles.css", "./js/index.js"];
 
 self.addEventListener("install", function (evt) {
@@ -36,15 +37,35 @@ self.addEventListener("activate", function (evt) {
 
 self.addEventListener('fetch', function (evt) {
   console.log('fetch request : ' + evt.request.url);
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+        .then(response => {
+          if (response.status===200) {
+            cache.put(evt.request.url, response.clone())
+          }
+          return response;
+        }).catch(err => {
+          return cache.match(evt.request);
+        })
+      }).catch(err => {
+        console.log(err);
+      })
+    )
+    return
+  }
   evt.respondWith(
-    caches.match(evt.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + evt.request.url);
-        return request;
-      } else {
-        console.log('file is not cached, fetching : ' + evt.request.url);
-        return fetch(evt.request);
-      }
-    })
+    fetch(evt.request).catch(function() {
+     return caches.match(evt.request).then(function (request) {
+        if (request) {
+          console.log('responding with cache : ' + evt.request.url);
+          return request;
+        } else {
+          console.log('file is not cached, fetching : ' + evt.request.url);
+          return fetch(evt.request);
+        }
+      })
+    })    
   );
 });
